64d0820fae3fb6d5e83f8dbdca5aa3ad16bef618
Leffers90 <mitchellleffers@gmail.com>
2019-01-28 22:34:11 +1000

    finished auth flow

--- config/prod.js
+++ config/prod.js
@@ -1,1 +1,9 @@
 // prod.js - production keys here!!!
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+
+mongodb://admin:admin123@ds149998.mlab.com:49998/emaily-prod
