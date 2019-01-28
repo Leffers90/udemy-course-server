f14598808d14d8a7302f0c391f071d08fc1cabf3
Mitchell Leffers <mitchell.leffers@gmail.com>
2019-01-28 21:12:58 +1000

    25%

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+// dev.js - don't commit this!!!
+
+module.exports = {
+    googleClientID:
+      "58873289009-5ed8ocl71s46ngt961roljadd1kmd2ua.apps.googleusercontent.com",
+    googleClientSecret: "LzMekmLgl8b2PlnkCO5VYO_p",
+    mongoURI: "mongodb://admin:admin123@dbh74.mlab.com:27747/emaily-dev",
+    cookieKey: "jndkfnskdnskjdnvjsndfankasjdnakfnkajnfs"
+   };
